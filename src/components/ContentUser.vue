1d6aba1ec3694d155226a93b6c59d62e3e7e78ab
Jerdirlson Santamaria <jerdirlson@gmail.com>
2024-09-22 15:56:23 -0500

    Feat/login connet (#1)
    * style(ContentUser): add space in content user
    
    * style(ContentUser): change the w-full in login card

--- src/components/ContentUser.vue
+++ src/components/ContentUser.vue
@@ -5,7 +5,7 @@ console.log(isVisible);
 </script>
 
 <template>
-  <div class="flex h-full">
+  <div class="flex h-full ">
     <ul class="flex pr-8 space-x-4 justify-center items-center gap-7">
       <li><a href="#" class="hover:text-gray-700">Home</a></li>
       <li><a href="#" class="hover:text-gray-700">About</a></li>
@@ -13,7 +13,7 @@ console.log(isVisible);
       <li><a href="#" class="hover:text-gray-700">Contact</a></li>
     </ul>
 
-    <div v-if="isVisible" id="image-user" class="flex flex-wrap items-center h-full w-full">
+    <div v-if="isVisible" id="image-user" class="flex flex-wrap items-center h-full w-1/6">
       <div class="flex h-3/4 w-full items-center justify-center">
 <!--        <div class="flex skeleton rounded-full bg-gray-300 w-10 h-10"></div>-->
         <img class="flex" src="/svg/chevron-direction-bottom-icon.svg" alt="icon"/>
