f30757f35e888b8f181130a73c3bd44757c536c1
Jerdirlson <jerdilson@outlook.es>
2024-10-20 15:37:52 -0500

    feat(fuck-patient): fix all module patient and create appointment

--- src/router/index.ts
+++ src/router/index.ts
@@ -20,7 +20,7 @@ const router = createRouter({
       path: '/dashboard',
       name: 'dashboard',
       component: () => import('../views/DashboardView.vue'),
-    },
+    },
     {
       path: '/patient',
       name: 'patient',
